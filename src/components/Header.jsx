2fd016330ccda47eee3cfa7d39175614fbb2360f
Anthony Johnson <anthony.johnson.now@gmail.com>
2026-01-17 14:39:17 -0800

    Fix mobile Back the Mission button - use consistent SVG heart icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -274,7 +274,6 @@ function Header({ onSecondaryScoutClick, onMyPacketClick, onCAPSimulatorClick, o
                 <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
               </svg>
               <span className="hidden md:inline">Back the Mission</span>
-              <span className="md:hidden">💚</span>
             </button>
           </nav>
         </div>
